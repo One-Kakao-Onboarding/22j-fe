5eb729b7265877ed2b522cd9bb1b92fbdcff9d79
astor-dev <orangnlp@gmail.com>
2026-01-13 11:06:29 +0900

    feat: api axios instance

---
+++ src/services/core/http-instance.ts
@@ -0,0 +1,125 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { z } from 'zod';
+import axios, { type AxiosRequestConfig } from 'axios';
+import { silentParse } from '@/services/core/silent-parse';
+import { CustomError } from '@/services/core/custom-error';
+import { ResultEnum, ErrorCodeEnum } from '@/constants/codes';
+
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+interface RequestInitWithSchema<T extends z.ZodTypeAny>
+  extends AxiosRequestConfig {
+  schema?: T;
+}
+
+const getApiResponseSchema = <T extends z.ZodTypeAny>(schema: T) =>
+  z
+    .object({
+      result: ResultEnum,
+      data: schema.nullable(),
+      message: z.string().nullable(),
+      errorCode: ErrorCodeEnum.nullable(),
+    })
+    .strict();
+
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+class Instance {
+    private readonly baseUrl: string;
+    constructor(baseUrl: string = BASE_URL ?? '') {
+      this.baseUrl = baseUrl;
+    }
+
+  // 기본 요청 함수
+  async fetchWithConfig<T extends z.ZodTypeAny>(
+    url: string,
+    method: HttpMethod,
+    body?: any,
+    options: RequestInitWithSchema<T> = {},
+  ) {
+    const { schema, ...pureOptions } = options;
+    const config: AxiosRequestConfig = {
+      method,
+      ...options,
+      withCredentials: true,
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        ...pureOptions?.headers,
+      },
+      ...(body && { data: body, ...pureOptions }),
+    };
+
+    const res = await axios(new URL(url, this.baseUrl).toString(), config);
+
+    const showToast = schema !== undefined && method === 'GET';
+    const responseSchema = schema
+      ? getApiResponseSchema(schema)
+      : getApiResponseSchema(z.object({}));
+
+    const parsedData: z.infer<typeof responseSchema> = silentParse(
+      responseSchema,
+      res.data,
+      { showToast },
+    );
+
+    if (parsedData.result === 'FAIL') {
+      console.error(parsedData.errorCode + ' ' + parsedData.message);
+      throw new CustomError(
+        parsedData.errorCode || 'UNKNOWN_ERROR_CODE',
+        parsedData.message || '알 수 없는 오류',
+      );
+    }
+
+    return parsedData;
+  }
+
+  async fetchWithRetry<T extends z.ZodTypeAny>(
+    url: string,
+    method: HttpMethod,
+    body?: any,
+    options: RequestInitWithSchema<T> = {},
+  ) {
+    const fetchOperation = () =>
+      this.fetchWithConfig<T>(url, method, body, options);
+    return await fetchOperation();
+  }
+
+
+  async get<T extends z.ZodTypeAny>(
+    url: string,
+    options?: RequestInitWithSchema<T>,
+  ) {
+    return this.fetchWithRetry<T>(url, 'GET', undefined, options);
+  }
+  async delete<T extends z.ZodTypeAny>(
+    url: string,
+    options?: RequestInitWithSchema<T>,
+  ) {
+    return await this.fetchWithRetry<T>(url, 'DELETE', undefined, options);
+  }
+  async post<T extends z.ZodTypeAny>(
+    url: string,
+    body: any,
+    options?: RequestInitWithSchema<T>,
+  ) {
+    return await this.fetchWithRetry<T>(url, 'POST', body, options);
+  }
+  async put<T extends z.ZodTypeAny>(
+    url: string,
+    body: any,
+    options?: RequestInitWithSchema<T>,
+  ) {
+    return await this.fetchWithRetry<T>(url, 'PUT', body, options);
+  }
+  async patch<T extends z.ZodTypeAny>(
+    url: string,
+    body: any,
+    options?: RequestInitWithSchema<T>,
+  ) {
+    return await this.fetchWithRetry<T>(url, 'PATCH', body, options);
+  }
+}
+
+export const instance = new Instance();
